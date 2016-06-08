2fd86558494ade73145fe264c49589aa5b1f360a
Shinya Yanagihara <shinya.com@gmail.com>
2016-06-08 11:56:16 +0000

    first commit

---
+++ attributes/default.rb
@@ -0,0 +1,7 @@
+default['weblogic']['owner'] = 'oracle'
+default['weblogic']['user'] = 'oracle'
+default['weblogic']['group'] = 'oinstall'
+default['weblogic']['mw_home'] = '/u01/app/oracle/product/fmw12.2.1'
+default['weblogic']['install_type'] = 'Fusion Middleware Infrastructure With Examples'
+default['weblogic']['response_file'] = '/root/install_weblogic.rsp'
+default['weblogic']['install_image'] = '/vagrant/test.sh'
